78672a109d305e2582df9da6536d207d25fb65dd
Nastya <an_vorotilina@mail.ru>
2022-06-11 06:30:33 +0300

    constructors were added

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -3,5 +3,23 @@
  * List containers comparator
  */
 public class Comparator {
+    /**
+     * Количество операций
+     */
+    private Integer countOperations = 10000;
 
+    /**
+     * Конструктор задающий количество операций
+     * @param countOperations Количество операций
+     */
+    public Comparator(Integer countOperations){
+        this.countOperations=countOperations;
+    }
+
+    /**
+     * Конструктор по умолчанию
+     */
+    public Comparator(){
+
+    }
 }
