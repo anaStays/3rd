ec0e55ef61e6f73f6d708ee6d72527a5dd0b2c2c
Nastya <an_vorotilina@mail.ru>
2022-06-11 06:57:07 +0300

    all tests was wrote

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -62,4 +62,38 @@ public class Comparator {
         }
         return System.currentTimeMillis() - timeStart;
     }
+
+    /**
+     * delete testing
+     * @param experimental experimental container
+     * @return testing time in ms
+     */
+    public long delete(List<Integer> experimental) {
+        long timeStart = System.currentTimeMillis();
+        for(int i = 0; i<countOperations;i++) {
+            experimental.remove(experimental.size()/2);
+        }
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    /**
+     * get testing
+     * @param experimental experimental container
+     * @return testing time in ms
+     */
+    public long get(List<Integer> experimental) {
+        long timeStart = System.currentTimeMillis();
+        for(int i = 0; i<countOperations;i++) {
+            Integer k = experimental.get(experimental.size()/2);
+        }
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    /**
+     * quantity of operations
+     * @return quantity of operations
+     */
+    public Integer getCountOperations() {
+        return countOperations;
+    }
 }
