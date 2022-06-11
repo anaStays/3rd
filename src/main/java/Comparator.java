efb82eab7542585c75ca923bddc2d7f4c875afeb
Nastya <an_vorotilina@mail.ru>
2022-06-11 06:46:47 +0300

    adding tests were created

--- src/main/java/Comparator.java
+++ src/main/java/Comparator.java
@@ -1,25 +1,65 @@
+import java.util.List;
 
 /**
  * List containers comparator
  */
 public class Comparator {
     /**
-     * Количество операций
+     * operations quantity
      */
     private Integer countOperations = 10000;
 
     /**
-     * Конструктор задающий количество операций
-     * @param countOperations Количество операций
+     * just constructor for operations counter
+     * @param countOperations operations quantity
      */
     public Comparator(Integer countOperations){
         this.countOperations=countOperations;
     }
 
     /**
-     * Конструктор по умолчанию
+     * default constructor
      */
     public Comparator(){
 
+    }
+
+    /**
+     * add testing method with middle inserting
+     * @param experimental experimental container
+     * @return testing time in ms
+     */
+    public long add(List<Integer> experimental) {
+        long timeStart = System.currentTimeMillis();
+        for(int i = 0; i<countOperations;i++) {
+            experimental.add(experimental.size()/2, i);
+        }
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    /**
+     * add in container begin
+     * @param experimental experimental container
+     * @return testing time in ms
+     */
+    public long addBegin(List<Integer> experimental){
+        long timeStart = System.currentTimeMillis();
+        for(int i = 0; i<countOperations;i++) {
+            experimental.add(0, i);
+        }
+        return System.currentTimeMillis() - timeStart;
+    }
+
+    /**
+     * add in container end
+     * @param experimental experimental container
+     * @return testing time in ms
+     */
+    public long addEnd(List<Integer> experimental){
+        long timeStart = System.currentTimeMillis();
+        for(int i = 0; i<countOperations;i++) {
+            experimental.add(i);
+        }
+        return System.currentTimeMillis() - timeStart;
     }
 }
